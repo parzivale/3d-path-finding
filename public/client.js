d8baf1eba73a769980754f5675899e9856adae38
parzivale <40600864+parzivale@users.noreply.github.com>
2021-10-26 15:13:23 +0800

    stole more code from three js examples

--- public/client.js
+++ public/client.js
@@ -1,62 +1,112 @@
-import * as THREE from 'three'
+import * as THREE from '../build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
+let camera, scene, renderer;
+let plane;
+let pointer, raycaster, isShiftDown = false;
+let rollOverMesh, rollOverMaterial;
+let cubeGeo, cubeMaterial;
+const objects = [];
 
-const scene = new THREE.Scene()
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.z = 2
-
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const controls = new OrbitControls(camera, renderer.domElement)
-
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener(
-    'resize',
-    () => {
-        camera.aspect = window.innerWidth / window.innerHeight
-        camera.updateProjectionMatrix()
-        renderer.setSize(window.innerWidth, window.innerHeight)
-        render()
-    },
-    false
-)
-
-const stats = Stats()
-document.body.appendChild(stats.dom)
-
-const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.scale, 'x', -5, 5)
-cubeFolder.add(cube.scale, 'y', -5, 5)
-cubeFolder.add(cube.scale, 'z', -5, 5)
-cubeFolder.open()
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 10)
-cameraFolder.open()
-
-function animate() {
-    requestAnimationFrame(animate)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-    controls.update()
-    render()
-    stats.update()
+init();
+render();
+function init() {
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+	camera.position.set(500, 800, 1300);
+	camera.lookAt(0, 0, 0);
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0xf0f0f0);
+	// roll-over helpers
+	const rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
+	rollOverMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true });
+	rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+	scene.add(rollOverMesh);
+	// cubes
+	cubeGeo = new THREE.BoxGeometry(50, 50, 50);
+	cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xfeb74c });
+	// grid
+	const gridHelper = new THREE.GridHelper(1000, 20);
+	scene.add(gridHelper);
+	//
+	raycaster = new THREE.Raycaster();
+	pointer = new THREE.Vector2();
+	const geometry = new THREE.PlaneGeometry(1000, 1000);
+	geometry.rotateX(- Math.PI / 2);
+	plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ visible: false }));
+	scene.add(plane);
+	objects.push(plane);
+	// lights
+	const ambientLight = new THREE.AmbientLight(0x606060);
+	scene.add(ambientLight);
+	const directionalLight = new THREE.DirectionalLight(0xffffff);
+	directionalLight.position.set(1, 0.75, 0.5).normalize();
+	scene.add(directionalLight);
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+	document.addEventListener('pointermove', onPointerMove);
+	document.addEventListener('pointerdown', onPointerDown);
+	document.addEventListener('keydown', onDocumentKeyDown);
+	document.addEventListener('keyup', onDocumentKeyUp);
+	//
+	window.addEventListener('resize', onWindowResize);
+	const controls = new OrbitControls(camera, renderer.domElement)
 }
-
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+function onPointerMove(event) {
+	pointer.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+	raycaster.setFromCamera(pointer, camera);
+	const intersects = raycaster.intersectObjects(objects, false);
+	if (intersects.length > 0) {
+		const intersect = intersects[0];
+		rollOverMesh.position.copy(intersect.point).add(intersect.face.normal);
+		rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+	}
+	render();
+}
+function onPointerDown(event) {
+	pointer.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+	raycaster.setFromCamera(pointer, camera);
+	const intersects = raycaster.intersectObjects(objects, false);
+	if (intersects.length > 0) {
+		const intersect = intersects[0];
+		// delete cube
+		if (isShiftDown) {
+			if (intersect.object !== plane) {
+				scene.remove(intersect.object);
+				objects.splice(objects.indexOf(intersect.object), 1);
+			}
+			// create cube
+		} else {
+			const voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+			voxel.position.copy(intersect.point).add(intersect.face.normal);
+			voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+			scene.add(voxel);
+			objects.push(voxel);
+		}
+		render();
+	}
+}
+function onDocumentKeyDown(event) {
+	switch (event.keyCode) {
+		case 16: isShiftDown = true; break;
+	}
+}
+function onDocumentKeyUp(event) {
+	switch (event.keyCode) {
+		case 16: isShiftDown = false; break;
+	}
+}
 function render() {
-    renderer.render(scene, camera)
+	renderer.render(scene, camera);
 }
 
+function animte(){
+	requestAnimationFrame(animate)
+	controls.update()
+}
 animate()
