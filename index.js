aef0b4eacf9481ff45d18f27d73f8309473031b0
parzivale <40600864+parzivale@users.noreply.github.com>
2021-10-26 14:43:44 +0800

    copied some guys tutorial on three js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path')
 
-const express = require('express');
-const app = express();              
-const port = 5000;                  
+app.use(express.static(__dirname + '/public'))
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-});
-
-app.listen(port, () => {
-    console.log(`Now listening on port ${port}`); 
-});
+app.listen(3000, () => console.log('Visit  http://127.0.0.1:3000'))
